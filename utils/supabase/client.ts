feca5940412b499ece5ff4b2e046ae72ed4fb99e
Hdensac <hdensa@gmail.com>
2026-08-02 22:45:20 +0100

    fix :  problème venait d'une configuration manquante au niveau de l'initialisation de Supabase

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,5 +1,8 @@
-﻿import { supabase } from "@/lib/supabase";
+import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-  return supabase;
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
 }
