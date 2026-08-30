8ee1c5e34e3e8a0b02eb598725683d342d9109ec
Hdensac <hdensa@gmail.com>
2026-08-30 16:41:18 +0100

    Add Sentry DSN config

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import * as Sentry from "@sentry/nextjs";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  Sentry.captureMessage("Sentry test event from homepage", "info");
   redirect("/dashboard");
 }
 
