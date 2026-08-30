c3f981ea6f90402671c6c49ab4cab42966314f50
Hdensac <hdensa@gmail.com>
2026-08-30 16:29:39 +0100

    feat(sentry): ajouter un message de test Sentry sur la page d'accueil

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import * as Sentry from "@sentry/nextjs";
 import { redirect } from "next/navigation";
 
 export default function Home() {
+  Sentry.captureMessage("Sentry test event from homepage", "info");
   redirect("/dashboard");
 }
 
